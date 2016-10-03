86b35a9b668009fe1d9643e3533539abb0b2c462
jcassarly <jaredcassarly@gmail.com>
2016-10-02 21:22:22 -0400

    added comment to show output format

--- print_vectors.cpp
+++ print_vectors.cpp
@@ -5,6 +5,7 @@
 
 /**
  * Prints out the x and y coordinates for each point in the array of vectors where the would be in a coordinate plane given they are in order
+ * outputs as (x,y)
  * Precondition: vector andlges must be in degrees
  * @param v the array of vectors to find the points in
  * @param length the length of the array of vectors (can calculate as sizeof(v)/sizeof(v[0]))
